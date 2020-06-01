5b70298437e6562224d00e2756a7ed4ce067b7a2
unknown <blacket021@gmail.com>
2020-06-01 03:27:24 -0300

    atualizados

--- commerce/app/Controller/getprodutos.php
+++ commerce/app/Controller/getprodutos.php
@@ -5,6 +5,19 @@ namespace App\Controller;
 class getPro{
     private $name;
     private $quantidade;
+
+    public function setName($name){
+       $this->name=$name;
+    }
+    public function setQuan($quantidade){
+        $this->quantidade=$quantidade;
+    }
+    public function getName(){
+        return $this->name;
+    }
+    public function getQuant(){
+        return $this->quantidade;
+    }
 }
 
 ?>
