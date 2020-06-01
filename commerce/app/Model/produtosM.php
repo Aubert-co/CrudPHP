742c44ca28c9b55e85568ebaa2d46f483b1334f0
unknown <blacket021@gmail.com>
2020-05-31 23:24:15 -0300

    atualizado

---
+++ commerce/app/Model/produtosM.php
@@ -0,0 +1,24 @@
+<?php 
+
+namespace App\model;
+use App\model\produtos;
+class crud{
+    function Select(){
+        $sql = "SELECT *FROM produtos";
+
+        $stmt = produtos::getConx()->prepare($sql);
+        $stmt->execute();
+
+        if($stmt->rowCount()<0){
+            return [];
+        }else{
+           $resultado = $stmt->fetchAll(\PDO::FETCH_ASSOC);
+           return $resultado;
+        }
+    }
+    function add(){
+        $sql = "INSERT INTO produtos('nome','quantidade') VALUES(?,?)";
+    }
+}
+
+?>
