4f9afc359a768379fad67381159d582e0ecb2b69
Black <53127242+Aubert-co@users.noreply.github.com>
2021-09-01 23:09:45 -0300

    Update getCon.php

--- crud/App/model/getCon.php
+++ crud/App/model/getCon.php
@@ -9,11 +9,11 @@ class GetCon{
    private static $instance;
  
    public static function Con(){
-       try{
-   if(!isset(self::$instance)){
-    self::$instance = new \PDO('mysql:host=localhost;dbname=catalago;','root','');
-    return self::$instance;
-}
+    try{
+        if(!isset(self::$instance)){
+            self::$instance = new \PDO('mysql:host=localhost;dbname=users;','root','');
+            return self::$instance;
+        }
        }catch(\Error $e){
            throw $e->getMessage();
        }
